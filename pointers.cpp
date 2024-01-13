4779ed457240c5437953ac0adeb3948b02ef0fab
r3hmantom <r3hmantom@gmail.com>
2024-01-13 19:10:10 +0500

    Task 2 + 3

--- pointers.cpp
+++ pointers.cpp
@@ -10,24 +10,50 @@ void swap( int* ptra, int* ptrb ) {
 
 int main() {
     
-    // Task 1
-    int a = 5, b = 6;
+    //TASK 1:
+    //Write the C++ code for a function swap(), which swaps the values of two integer variables.
+    //Note : you cannot create global variables, and cannot pass integer variables by reference in the function.
+    
+    //int a = 5, b = 6;
 
-    int* ptra = &a;
-    int* ptrb = &b;
+    //int* ptra = &a;
+    //int* ptrb = &b;
 
-    cout << "a: " << a << endl;
-    cout << "b: " << b << endl;
-    swap(ptra, ptrb);
-    cout << "a: " << a << endl;
-    cout << "b: " << b << endl;
+    //cout << "a: " << a << endl;
+    //cout << "b: " << b << endl;
+    //swap(ptra, ptrb);
+    //cout << "a: " << a << endl;
+    //cout << "b: " << b << endl;
+
+    // ----------------------------------------------------
+
+    //TASK 2:
+    //Given two integers x and y, find their sum using pointers.
+    //int x = 5, y = 5;
+    //int* ptrx = &x;
+    //int* ptry = &y;
+
+    //int sum = *ptrx + *ptry;
+    //cout << "Sum of " << x << " and " << y << " is: " << sum << endl;
+
+       
+    // ----------------------------------------------------
+
+    // TASK 3:
+    //int* ptr = new int;
+    //cout << "Enter Int Value: ";
+    //cin >> *ptr;
+    //cout << "Value is: " << *ptr << endl;
+    //cout << "Pointer Value is: " << ptr << endl;
+    //int* ptr1 = new int;
+    //*ptr1 = 9;
+    //cout << *ptr1 << endl;
+    //ptr1++;
+    //cout << *ptr1 << endl;
+    //cout << "Find Memory Leak" << endl;
+
 
     return 0;
 }
 
-
-//TASK 1:
-//Write the C++ code for a function swap(), which swaps the values of two integer variables.
-//Note : you cannot create global variables, and cannot pass integer variables by reference in the function.
-//Hint : Pointers
 
