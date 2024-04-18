92fb13d3d2ae6c4f8089a4118e52778528c0c48f
r3hmantom <r3hmantom@gmail.com>
2024-04-18 23:54:42 +0500

    question 2

--- DryRuns.cpp
+++ DryRuns.cpp
@@ -1,19 +1,36 @@
-#include <iostream>
+//#include <iostream>
+//using namespace std;
+//
+//
+//void mystery(int* ptr, int s)
+//{
+//    ptr = new int[s];
+//    for (int i = 0, j = s; i < s; ++i, j--)
+//        *(ptr + i) = j;
+//}
+//int main()
+//{
+//    int* ptr, s = 5;
+//    mystery(ptr, s);
+//    for (int i = 0; i < s; ++i)
+//        cout << ptr[i] << " ";
+//    delete[] ptr; ptr = NULL;
+//    return 0;
+//}
+
+#include<iostream>
 using namespace std;
-
-
-void mystery(int* ptr, int s)
+main()
 {
-    ptr = new int[s];
-    for (int i = 0, j = s; i < s; ++i, j--)
-        *(ptr + i) = j;
-}
-int main()
-{
-    int* ptr, s = 5;
-    mystery(ptr, s);
-    for (int i = 0; i < s; ++i)
-        cout << ptr[i] << " ";
-    delete[] ptr; ptr = NULL;
-    return 0;
+    int* ip = new int;
+    short* sp;
+    char* cp;
+    *ip = 16706; //Hex 4142
+    *ip = 65;
+    //cp=ip;
+    cp = (char*)ip;
+    cout << *cp << endl;
+    cout << *(cp + 1) << endl;
+    sp = (short*)ip;
+    cout << *sp;
 }
